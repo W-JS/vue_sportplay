b143b62fe9cdcadb4338d594e93643ac1cea1051
W_JS <wanjinshengwjs@163.com>
2020-09-26 13:32:12 +0800

    Signed-off-by: W_JS <wanjinshengwjs@163.com>

---
+++ src/components/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>欢迎使用个人运动管理平台！！！</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    // 132
+}
+</script>
+
+<style lang="less" scoped>
+// 12
+</style>
